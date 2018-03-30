3764a0a58105d4f4f9e12747c73542e2d52fc37c
howardwang15 <joy_luck_2000@hotmail.com>
2018-03-29 17:35:54 -0700

    fixed the cross entropy error to properly calculate the error between training batch and one hot labels

--- neural_net.py
+++ neural_net.py
@@ -33,10 +33,14 @@ def sigmoid(x): #sigmoid activation function
 
 
 def softmax(x):
-    a = []
+    b = []
     for i in x:
-      a.append(np.exp(i)/float(np.sum(np.exp(x))))
-    return np.asarray(a)
+        a = []
+        temp = i
+        for j in i:
+            a.append(np.exp(j)/float(np.sum(np.exp(i))))
+        b.append(a)
+    return np.asarray(b)
 
 
 def create_weights(input_size, output_size):
@@ -51,18 +55,17 @@ def create_bias(output_size):
 
 def cross_entropy_loss(pred, actual):
     pred = pred.tolist()
-    for i in pred:
-        if i == 0:
-            print("WHY")
-    return -np.mean(actual * np.log(pred)/np.log(10))
+    losses = []
+    for i in range(len(pred)):
+        avg_loss = -np.mean(actual[i] * np.log(pred[i])/np.log(10))
+        losses.append(avg_loss)
+    losses = np.asarray(losses)
 
-
-def mean_squared_error(pred, actual):
-    return np.mean((pred - actual)**2)
+    return np.mean(losses)
 
 
 class Model:
-    def __init__(self, inputs):
+    def __init__(self):
         self.w1 = create_weights(n_pixels, hidden_layer1_neurons)
         self.b1 = create_bias(hidden_layer1_neurons)
 
@@ -72,6 +75,8 @@ class Model:
         self.w3 = create_weights(hidden_layer2_neurons, n_classes)
         self.b3 = create_bias(n_classes)
 
+
+    def predict(self, inputs):
         self.h1 = np.dot(inputs, self.w1) + self.b1
         self.h1 = sigmoid(self.h1)
 
@@ -79,28 +84,27 @@ class Model:
         self.h2 = sigmoid(self.h2)
 
         self.output = np.dot(self.h2, self.w3) + self.b3
-        self.output = sigmoid(self.output)
-        print(self.output)
+        self.output = softmax(self.output)
+
+        return self.output
+
 
 
 images = np.asarray(images)
-model = Model(images)
+
+train_batch = []  # select images to be trained
+batch_labels = []  # the labels of those images
+for j in range(batch_size):
+    random_num = random.randrange(0, images.shape[0])
+    train_batch.append(images[random_num])
+    batch_labels.append(one_hot_encoding(labels[random_num]))
+
+prediction = Model()
+
+for i in range(n_epochs): #train for specified num of epochs
+    output = prediction.predict(train_batch)
+
+    loss = cross_entropy_loss(output, batch_labels)
+    assert(len(train_batch) == len(batch_labels))
 
 
-for i in range(n_epochs):
-    train_batch = []
-    batch_labels = []
-    for j in range(batch_size):
-        random_num = random.randrange(0, images.shape[0])
-        train_batch.append(images[random_num])
-        batch_labels.append(labels[random_num])
-
-    train_batch = np.asarray(train_batch)
-    batch_labels = np.asarray(batch_labels)
-    for j in range(len(train_batch)):
-        one_hot_label = np.asarray(one_hot_encoding(batch_labels[j]))
-        error = cross_entropy_loss(softmax(model.output), one_hot_label)
-        #print(error)
-        if j % batch_size == 0:
-            break
-            #print("hi")
