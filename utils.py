c4f31d68c23eeb7144a7c35e640a9f1db6889f42
howardwang15 <joy_luck_2000@hotmail.com>
2019-01-03 23:56:45 -0800

    added sigmoid prime function in utils for backpropagation, added gradient computation WRT weights in the last layer

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import numpy as np
 def sigmoid(x):
     return 1.0/(1.0+np.exp(-x))
 
+def sigmoid_prime(x):
+    return sigmoid(x) * (1-sigmoid(x))
+
 def softmax(prediction):
     return np.exp(prediction)/np.sum(np.exp(prediction))
 
