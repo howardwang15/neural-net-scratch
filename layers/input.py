1943ecb97726f7651ca7cbb8426fa738e763436b
Howard Wang <joy_luck_2000@hotmail.com>
2021-05-16 00:18:40 -0700

    Add input layer (#6)
    * add gitignore
    
    * added input layer

---
+++ layers/input.py
@@ -0,0 +1,13 @@
+from layers import Layer
+
+class Input(Layer):
+  def __init__(self, shape, **kwargs):
+    super().__init__(**kwargs)
+    self._shape = shape
+
+  def forward(self, inputs):
+      return inputs
+
+  @property
+  def output_shape(self):
+    return self._shape
