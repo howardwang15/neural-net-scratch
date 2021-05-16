6d7c5a8b27f399314e3ab360c9090ccab1e3910f
howardwang15 <joy_luck_2000@hotmail.com>
2021-05-16 00:17:20 -0700

    added input layer

--- layers/layer.py
+++ layers/layer.py
@@ -2,12 +2,12 @@ class Layer:
   def __init__(self, name):
     self._name = name
 
-  @property
-  def name(self):
-    return self._name
-
   def forward(self, inputs):
     pass
 
   def backprop(self):
     pass
+
+  @property
+  def name(self):
+    return self._name
