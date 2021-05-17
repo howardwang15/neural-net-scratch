d0c9f0e8aec75a4bf3e86339791e0fd42ab40bab
howardwang15 <joy_luck_2000@hotmail.com>
2021-05-16 22:46:07 -0700

    add validation for inputs to forward()

--- core/layers/layer.py
+++ core/layers/layer.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Layer:
   def __init__(self, name):
     self._name = name
@@ -7,6 +9,16 @@ class Layer:
 
   def backprop(self):
     pass
+
+  def _validate(self, inputs):
+    if not isinstance(inputs, np.ndarray):
+      raise ValueError('Inputs must be of type numpy.ndarray')
+
+    if inputs.dtype not in ['int32', 'int64', 'float32', 'float64']:
+      raise ValueError('Inputs must be of type int32, int64, float32, float64')
+
+    if len(inputs.shape) < 2:
+      raise ValueError('Inputs must be of at least dimension 2 (including batch)')
 
   @property
   def name(self):
